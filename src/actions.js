5312c3bea11991b217256dd4198243af0f9ca042
Andrew Cox <drew.r.cox@gmail.com>
2017-11-01 15:32:04 -0400

    Initial Commit

---
+++ src/actions.js
@@ -0,0 +1,4 @@
+export const TOGGLE_INFO_MODAL = 'TOGGLE_INFO_MODAL';
+export const toggleInfoModal = () => ({
+    type: TOGGLE_INFO_MODAL
+});
---
+++ src/reducer.js
@@ -0,0 +1,5 @@
+import { TOGGLE_INFO_MODAL } from './actions';
+
+const initialState = {};
+
+export default (state, action) => {};
