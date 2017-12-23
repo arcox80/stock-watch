5c6ca143e2177ffa76f1a41a10cce19733ae87e9
Andrew Cox <drew.r.cox@gmail.com>
2017-12-22 23:59:24 -0500

    Reorganize project to be able to deploy on Heroku and make style changes

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const request = require("request");
 const http = require("http");
-const {API_KEY, PORT} = require("./config");
-
 const app = express();
-app.use(express.static("./"));
+
+app.use(express.static("client"));
+
+app.get("/data/:query", (req, res) => {
+  let url = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${req.params.query}&apikey=S3H1N4O2750Y09RN`;
+  request.get({ url: url, headers: {"content-type": "application/json"} }).pipe(res);
+});
 
 app.get("/headlines/:query", (req, res) => {
   let url = `https://finance.yahoo.com/rss/headline?s=${req.params.query}&region=US&lang=en-US`;
   request.get({ url: url, headers: {"content-type": "text/xml"} }).pipe(res);
 });
 
-app.get("/data/:query", (req, res) => {
-  let url = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${req.params.query}&apikey=${API_KEY}`;
-  request.get({ url: url, headers: {"content-type": "application/json"} }).pipe(res);
-});
-
 app.use("*", function (req, res) {
   return res.status(404).json({ message: "Not Found" });
 });
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}`); });
